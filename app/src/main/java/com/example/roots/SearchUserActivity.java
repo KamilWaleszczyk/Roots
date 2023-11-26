630d2bd9428599acbad9cfdac91eb749699b3951
Kamil Waleszczyk <kamil.wk.23@gmail.com>
2023-11-26 18:39:11 +0100

    26.11

--- app/src/main/java/com/example/roots/SearchUserActivity.java
+++ app/src/main/java/com/example/roots/SearchUserActivity.java
@@ -1,14 +1,46 @@
 package com.example.roots;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class SearchUserActivity extends AppCompatActivity {
+
+    EditText searchInput;
+    ImageButton searchButton;
+    ImageButton backButton;
+    RecyclerView recyclerView;
+
+   // SearchUserRecyclerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_user);
+
+        searchInput = findViewById(R.id.seach_username_input);
+        searchButton = findViewById(R.id.search_user_btn);
+        backButton = findViewById(R.id.back_btn);
+        recyclerView = findViewById(R.id.search_user_recycler_view);
+
+        searchInput.requestFocus(); // edit text will be focused, keyword should open
+
+
+        backButton.setOnClickListener(v -> {
+            onBackPressed();
+        });
+
+        searchButton.setOnClickListener(v -> {
+            String searchTerm = searchInput.getText().toString();
+            if(searchTerm.isEmpty() || searchTerm.length()<3){
+                searchInput.setError("Zła Nazwa Użytkownika");
+                return;
+            }
+        //    setupSearchRecyclerView(searchTerm);
+        });
     }
 }
